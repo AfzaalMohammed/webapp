ba83977f2f7ac717e03997881bc83760592bf2da
AfzaalMohammed <afzaal.ahbar@gmail.com>
2018-05-15 15:53:29 -0400

    first commit

---
+++ webapp.go
@@ -0,0 +1,12 @@
+package main 
+
+import ("fmt"
+         "net/http")
+
+         func index_handler(w http.ResponseWriter, r *http.Request){
+         	fmt.Fprintf(w,"Hello this is Afzaal")
+         }
+         func main(){
+         	http.HandleFunc("/", index_handler)
+         	http.ListenAndServe(":8000",nil)
+         }
